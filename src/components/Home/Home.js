cbbb7eb5ad7fd20a2faec967ee1a469728ff24f7
saif-sudo <mdalisaif4@gmail.com>
2022-10-21 15:30:45 +0600

    project done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
       
             <img className='headerimg' src="https://img.freepik.com/free-vector/quiz-neon-sign_1262-19629.jpg?w=2000" alt="quiz"  />
             <h3>Welcome To Quiz-Pro</h3>
-            <p>This is a Quiz Related Site - Everyone need this for test</p>
+            <p fontSize={24}>This is a Quiz Related Site - Everyone need this for test</p>
         <div className="quizsection">
         {
                
